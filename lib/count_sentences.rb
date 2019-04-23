8cc662cabeb18ef15b2b799408a9f0907d04f373
github username <github email address>
2019-04-23 15:55:47 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
   end
+end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
   end
+end
 
   def count_sentences
-
+  self.squeeze('.!?').count('.!?')
   end
 end
